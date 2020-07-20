1ab380f9341f4e6bb8b11bf0123ceb8e021e3269
m3r71n <54747628+m3r71n@users.noreply.github.com>
2020-07-20 13:27:09 -0400

    fixed modal images

--- scripts/modalImages.js
+++ scripts/modalImages.js
@@ -2,10 +2,10 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.modalImg');
+var img = $('.myImg');
 var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-$('.modalImg').click(function(){
+$('.myImg').click(function(){
     modal.style.display = "block";
     var newSrc = this.src;
     modalImg.attr('src', newSrc);
